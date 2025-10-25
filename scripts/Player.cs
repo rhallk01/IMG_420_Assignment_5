2e69eb28b42f44e7f75068f880ca10f6fb2923a2
Haley Kloss <hrk85@nau.edu>
2025-10-25 15:34:40 -0700

    updated player starter script

--- scripts/Player.cs
+++ scripts/Player.cs
@@ -5,14 +5,16 @@ public partial class Player : CharacterBody2D
 
  public override void _PhysicsProcess(double delta)
  {
- // TODO: Implement basic movement (WASD or Arrow keys)
- // TODO: Use MoveAndSlide()
-
- Vector2 velocity = Velocity;
-
- // Get input and move
-
- Velocity = velocity;
- MoveAndSlide();
+	// TODO: Implement basic movement (WASD or Arrow keys)
+	// TODO: Use MoveAndSlide()
+	Vector2 velocity = Vector2.Zero;
+		
+	// Get input and move
+	if (Input.IsActionPressed("ui_right")) velocity.X += 1;
+	if (Input.IsActionPressed("ui_left")) velocity.X -= 1;
+	if (Input.IsActionPressed("ui_down")) velocity.Y += 1;
+	if (Input.IsActionPressed("ui_up")) velocity.Y -= 1;
+	Velocity = velocity.Normalized() * Speed;
+	MoveAndSlide();
  }
 }
